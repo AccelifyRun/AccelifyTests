8fcd01d2ae34f67c8fedb45ac08fbf77bb9da006
Iakov Volf <jakoff@gmail.com>
2016-07-14 20:57:56 +0300

    Coded step

--- AccelifyTraining/CodedTests/CheckPartialFilter.tstest.cs
+++ AccelifyTraining/CodedTests/CheckPartialFilter.tstest.cs
@@ -49,11 +49,31 @@ namespace AccelliTrackUni
         [CodedStep(@"New Coded Step")]
         public void CheckPartialFilter_CodedStep()
         {
-            HtmlTable table = Find.ByExpression<HtmlTable>("id=pnlStudentList" , "|" , "role=rowgroup");
-            foreach (HtmlTableRow row in table.AllRows) {
-                String text = row.Cells[1].TextContent;
-                Log.WriteLine("Student name in row is "+text);
+            
+            HtmlTable table1 = Find.ByExpression<HtmlTable>("tagname=table", "role=grid");
+            int r = table1.Rows.Count;
+           // object myData = GetExtractedValue("letters");
+           // string letters = Convert.ToString(myData);
+    object myData = Data["Let"];
+            string letters = Convert.ToString(myData);
+                for (int i = 1; i < r; i++)
+{
+    HtmlTableRow row = table1.Rows[i];
+    //ToDo change cell index to 2 for last name checking
+    HtmlTableCell cell = row.Cells[1];
+    string fullname = cell.TextContent;
+    Log.WriteLine("Student full name is "+fullname);
+     string[] name = fullname.Split(',');
+            string FirstName = name[1];
+            FirstName = FirstName.Trim();
+            string LastName = name[0];
+    int rownumberh = i+1;
+            Log.WriteLine("Student first name in row number "+rownumberh+" is "+FirstName);
+    Log.WriteLine("Student last name in row number "+rownumberh+" is "+LastName);
+    //Assert.IsTrue(FirstName.Contains(letters));
+    Assert.IsTrue(FirstName.StartsWith(letters));
+}
             }
         }
-    }
+
 }
